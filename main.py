cde2be5c3c019f9407b1ff53805a244a33a38a39
Azzam <varokahdotnet@gmail.com>
2021-06-17 15:35:06 +0800

    Polymorphism

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ print(p1.hitung_luas())
 s1 = Segitiga(10, 3)
 print(s1.info())
 print(s1.hitung_luas())
+
+#Polymorphism: kemampuan objek untuk merespon berbeda terhadap pemanggilan metod yang sama
+daftar_bangun_ruang = []
+daftar_bangun_ruang.append(p1)
+daftar_bangun_ruang.append(s1)
+
+print('\nPolimorfisme')
+for bangun_ruang in daftar_bangun_ruang:
+    print(bangun_ruang.info())
+
